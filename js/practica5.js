fad43897a732c152e03e6c4fd3058e613c6dbe9e
Cesar Narvaez <Cesar.teacher24@gmail.com>
2023-02-21 03:12:04 -0500

    Commit Practica

--- js/practica5.js
+++ js/practica5.js
@@ -1,10 +1,13 @@
 "uses strict"
 
-var datoA = 10;
-var datoB = 20;
+var datoA = 11;
+var datoB = 2;
 
 
 // var and = (datoA > && datoB >10)
+
+var residuo = datoA % datoB
+console.log(residuo)
 
 var or = (datoA >= 10 || datoB > 5)
 
@@ -35,3 +38,30 @@ console.log(portIgual)
 var divisionIgual = 20
 divisionIgual /= datoA
 console.log(divisionIgual)
+
+
+// ----------------concatenacion-----------------
+
+var nombre ="Sergio"
+var apellido = "Narvaez"
+
+var concatenacionTexto = nombre + " " + apellido
+console.log(concatenacionTexto)
+
+
+// ---------------operador ternario--------------
+
+// es hacer una condicional pero con la necesidad de eniar los resultados booleanos a una variable
+var resultado = datoA > datoB ? "si es mayor ":"No es mayor"
+console.log(resultado)
+
+// -------------Tipo de datos-------------
+
+var activo = true;
+
+var persona = {
+    numero : 10,
+    deporte: "Correr"
+}
+
+console.log(typeof persona.numero)
